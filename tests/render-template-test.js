95e630065722ba4e6fcc8aff24a6733715a2cb4b
Rod Vagg <rod@vagg.org>
2013-08-04 20:53:34 +1000

    added presentation archetype

---
+++ tests/render-template-test.js
@@ -0,0 +1,8 @@
+const renderTemplate = require('../lib/render-template')
+    , buster         = require('bustermove')
+    , assert         = require('referee').assert
+    , refute         = require('referee').refute
+
+buster.testCase('render-template', {
+  
+})
